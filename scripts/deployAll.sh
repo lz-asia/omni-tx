79318d21daf18520dc350342617df678f1033f2c
LevX <team@levx.io>
2023-03-02 03:40:14 +0900

    Deploy to polygon, arbitrum and optimism

--- scripts/deployAll.sh
+++ scripts/deployAll.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# yarn run hardhat deploy --network ethereum &&\
-# yarn run hardhat deploy --network bsc &&\
-# yarn run hardhat deploy --network avalanche &&\
-# yarn run hardhat deploy --network polygon &&\
-# yarn run hardhat deploy --network arbitrum &&\
-# yarn run hardhat deploy --network optimism &&\
-# yarn run hardhat deploy --network fantom &&\
+#yarn run hardhat deploy --network mainnet &&\
+#yarn run hardhat deploy --network bsc &&\
+#yarn run hardhat deploy --network avalanche &&\
+yarn run hardhat deploy --network polygon &&\
+yarn run hardhat deploy --network arbitrum &&\
+yarn run hardhat deploy --network optimism #&&\
+#yarn run hardhat deploy --network fantom #&&\
 yarn run hardhat deploy --network goerli &&\
 yarn run hardhat deploy --network bsc-testnet &&\
 yarn run hardhat deploy --network fuji &&\
