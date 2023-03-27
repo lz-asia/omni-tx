9f414ab1b45636157418006cd9ec692e186ded45
LevX <team@levx.io>
2023-03-27 20:58:47 +0900

    Deploy to mainnets

--- scripts/mainnet:setup.sh
+++ scripts/mainnet:setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-yarn run hardhat run scripts/setup.ts --network mainnet &&\
-yarn run hardhat run scripts/setup.ts --network bsc &&\
-yarn run hardhat run scripts/setup.ts --network avalanche &&\
-yarn run hardhat run scripts/setup.ts --network polygon &&\
-yarn run hardhat run scripts/setup.ts --network arbitrum &&\
-yarn run hardhat run scripts/setup.ts --network optimism &&\
+#yarn run hardhat run scripts/setup.ts --network ethereum &&\
+#yarn run hardhat run scripts/setup.ts --network bsc &&\
+#yarn run hardhat run scripts/setup.ts --network avalanche &&\
+#yarn run hardhat run scripts/setup.ts --network polygon &&\
+#yarn run hardhat run scripts/setup.ts --network arbitrum &&\
+#yarn run hardhat run scripts/setup.ts --network optimism &&\
 yarn run hardhat run scripts/setup.ts --network fantom
