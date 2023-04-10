41dd527921c4b4bb37050425d083095881eb6e60
LevX <team@levx.io>
2023-04-10 09:15:15 +0900

    Fix scripts

--- scripts/fork.sh
+++ scripts/fork.sh
@@ -19,24 +19,38 @@ $HARDHAT node --config hardhat-fork/fantom.config.ts --port 8250 1>/dev/null&
 sleep 5
 echo "✅ successfully forked mainnets"
 
+echo "⌛️️ ethereum"
 rm -rf deployments/ethereum-fork && ($HARDHAT deploy --network ethereum-fork || true)
+echo "⌛️️ bsc"
 rm -rf deployments/bsc-fork && ($HARDHAT deploy --network bsc-fork || true)
+echo "⌛️️ avalanche"
 rm -rf deployments/avalanche-fork && ($HARDHAT deploy --network avalanche-fork || true)
+echo "⌛️️ polygon"
 rm -rf deployments/polygon-fork && ($HARDHAT deploy --network polygon-fork || true)
+echo "⌛️️ arbitrum"
 rm -rf deployments/arbitrum-fork && ($HARDHAT deploy --network arbitrum-fork || true)
+echo "⌛️️ optimism"
 rm -rf deployments/optimism-fork && ($HARDHAT deploy --network optimism-fork || true)
+echo "⌛️️ fantom"
 rm -rf deployments/fantom-fork && ($HARDHAT deploy --network fantom-fork || true)
 
+echo "⌛️️ ethereum"
 $HARDHAT run scripts/setup.ts --network ethereum-fork &&\
+echo "⌛️️ bsc"
 $HARDHAT run scripts/setup.ts --network bsc-fork &&\
+echo "⌛️️ avalanche"
 $HARDHAT run scripts/setup.ts --network avalanche-fork &&\
+echo "⌛️️ polygon"
 $HARDHAT run scripts/setup.ts --network polygon-fork &&\
+echo "⌛️️ arbitrum"
 $HARDHAT run scripts/setup.ts --network arbitrum-fork &&\
+echo "⌛️️ optimism"
 $HARDHAT run scripts/setup.ts --network optimism-fork &&\
+echo "⌛️️ fantom"
 $HARDHAT run scripts/setup.ts --network fantom-fork
 
 clear() {
-  kill -9 "$(ps ax | grep 'hardhat-fork node' | awk '{print $1}')"
+  ps ax | grep "$HARDHAT" | grep -v "grep" | awk '{print $1}' | xargs kill -9
 }
 trap clear EXIT
 wait
--- scripts/setup.ts
+++ scripts/setup.ts
@@ -2,39 +2,41 @@ import fs from "fs";
 import { ethers, network } from "hardhat";
 import chainIds from "../constants/chainIds.json";
 import { mainnet, testnet } from "../constants/networks.json";
-import { StargateProxy } from "../typechain-types";
+import { OmniTx } from "../typechain-types";
 import { utils } from "ethers";
 
-function getStargateProxyAddress(network) {
-    const { address } = JSON.parse(
-        fs.readFileSync("deployments/" + network + "/StargateProxy.json", { encoding: "utf-8" })
-    );
+function getOmniTxAddress(network) {
+    const { address } = JSON.parse(fs.readFileSync("deployments/" + network + "/OmniTx.json", { encoding: "utf-8" }));
     return address;
 }
 
 async function setup(networks) {
-    const proxy = (await ethers.getContractAt("StargateProxy", getStargateProxyAddress(network.name))) as StargateProxy;
+    const omniTx = (await ethers.getContractAt("OmniTx", getOmniTxAddress(network.name))) as OmniTx;
     for (const networkName of networks) {
         if (networkName == network.name) continue;
         if (fs.existsSync("deployments/" + networkName)) {
-            const addr = utils.getAddress(await proxy.dstAddress(chainIds[networkName]));
-            const dstProxy = utils.getAddress(getStargateProxyAddress(networkName));
-            if (addr == dstProxy) {
+            const addr = utils.getAddress(await omniTx.dstAddress(chainIds[networkName]));
+            const dst = utils.getAddress(getOmniTxAddress(networkName));
+            if (addr == dst) {
                 console.log("reusing dst address for " + networkName);
             } else {
                 console.log("updating dst address for " + networkName);
-                await proxy.updateDstAddress(chainIds[networkName], dstProxy);
+                await omniTx.updateDstAddress(chainIds[networkName], dst);
             }
         }
     }
 }
 
 async function main() {
-    if (mainnet.includes(network.name)) {
-        console.log("Setting up StargateProxy for mainnets");
+    let networkName = network.name;
+    if (networkName.endsWith("-fork")) {
+        networkName = networkName.substring(0, networkName.length - 5);
+    }
+    if (mainnet.includes(networkName)) {
+        console.log("Setting up OmniTx for mainnets");
         await setup(mainnet);
-    } else if (testnet.includes(network.name)) {
-        console.log("Setting up StargateProxy for testnets");
+    } else if (testnet.includes(networkName)) {
+        console.log("Setting up OmniTx for testnets");
         await setup(testnet);
     }
 }
