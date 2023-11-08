af7ba84603317b5c0026d12a9b9e29e4c053884d
LevX <team@levx.io>
2023-11-08 18:16:44 +0900

    Bump @lz-kit/cli to 0.7.2

--- scripts/configOmniTx.ts
+++ scripts/configOmniTx.ts
@@ -1,27 +1,25 @@
-import { getEndpointAddress, getForkedChainId, getLZChainId } from "@lz-kit/cli";
-import { utils } from "ethers";
-import { getChain } from "hardhat";
-import { IOmniTx } from "../typechain-types";
+import "@lz-kit/cli"
+import { utils } from "ethers"
+import { getChain } from "hardhat"
+import { IOmniTx } from "../typechain-types"
 
 async function main() {
-    const chain = await getChain(process.env.LZ_KIT_SRC);
-    const dest = await getChain(process.env.LZ_KIT_DEST);
-    const { address } = await dest.getContract<IOmniTx>("OmniTx");
+    const chain = await getChain(process.env.LZ_KIT_SRC)
+    const dest = await getChain(process.env.LZ_KIT_DEST)
+    const { address } = await dest.getContract<IOmniTx>("OmniTx")
 
-    const contract = await chain.getContract<IOmniTx>("OmniTx", (await chain.getSigners())[0]);
-    const chainId = await getForkedChainId(dest.provider, true);
-    const lzChainId = await getLZChainId(getEndpointAddress(chainId), dest.provider);
-    const prevAddr = utils.getAddress(await contract.dstAddress(lzChainId));
-    const newAddr = utils.getAddress(address);
+    const contract = await chain.getContract<IOmniTx>("OmniTx", (await chain.getSigners())[0])
+    const prevAddr = utils.getAddress(await contract.dstAddress(dest.lzChainId))
+    const newAddr = utils.getAddress(address)
     if (prevAddr == newAddr) {
-        console.log("👀 Reusing dstAddress for " + dest.name);
+        console.log("👀 Reusing dstAddress for " + dest.name)
     } else {
-        console.log("✅ Updating dstAddress for " + dest.name);
-        await contract.updateDstAddress(chainId, newAddr);
+        console.log("✅ Updating dstAddress for " + dest.name)
+        await contract.updateDstAddress(dest.provider.network.chainId, newAddr)
     }
 }
 
 main().catch(error => {
-    console.error(error);
-    process.exitCode = 1;
-});
+    console.error(error)
+    process.exitCode = 1
+})
